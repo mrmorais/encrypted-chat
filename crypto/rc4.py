8a8563d481f2737a007070217c72977b6218d816
Maradona Morais <maradona.morais@hotmail.com>
2019-09-06 10:41:34 -0300

    :package: RC4 with tests

--- crypto/rc4.py
+++ crypto/rc4.py
@@ -1,22 +1,38 @@
-# Rivest Cipher 4 - RC4
+# Rivest Cipher 4 (RC4) Implementation
+
+"""
+How it works? (a better explanation you can find on Wikipedia article https://en.wikipedia.org/wiki/RC4)
+
+RC4 is a stream cipher algorithm, it's mainly based on 2 steps:
+
+a) Generate a initial permuted state which is based on a provided key. The key must have
+a length up to 256 bytes.
+
+b) For each interation over a pseudo-random generator a value is return to be used for encrypt
+1 byte by applying a bitwise exclusive (XOR) generating a ciphertext or plaintext. This process requires
+variables i and j to be stored.
+"""
 
 class RC4:
-    i = 0
-    j = 0
-
     def __init__(self, key):
         """
+        Set initial attributes and calculates the permutation state array.
         """
+
+        self.i = 0
+        self.j = 0
         self.key = key
-        self.state = self.key_scheduling_algorithm()
+        self.state = self.key_scheduling()
 
-    def key_scheduling_algorithm(self):
+    def key_scheduling(self):
         """
-        Initialize the permutation in the state array
-        based on key and key length
+        Initialize the permutation state in the identity permuted state array,
+        it's based on key and key length.
+
+        Reference: https://en.wikipedia.org/wiki/RC4
         """
 
-        state = range(256) # Initializing state vector [0..255]
+        state = list(range(256)) # Initializing state vector [0..255]
         j = 0
         for i in range(256):
             j = (j + state[i] + ord(self.key[i % len(self.key)])) % 256
@@ -24,21 +40,70 @@ class RC4:
 
         return state
 
-    def pseudo_random_generator(self):
+    def reset(self, key):
         """
+        Reset the algorithm state and recalculates the permutation state array based on a new key.
         """
+
+        self.i = 0
+        self.j = 0
+        self.key = key
+        self.state = self.key_scheduling()
+
+    def gen_pseudo_random(self):
+        """
+        PRGA (Pseudo-random generation algorithm) implementation. It returns the
+        next 8 bits sub-key.
+        """
+
         self.i = (self.i + 1) % 256
         self.j = (self.j + self.state[self.i]) % 256
         self.state[self.i], self.state[self.j] = self.state[self.j], self.state[self.i]
         
         return self.state[(self.state[self.i] + self.state[self.j]) % 256]
 
-    def next():
+    def apply(self, text, i_format='ascii', o_format='hex'):
         """
-        """
-
-    def encrypt(key):
-        """
+        Encrypt or Decrypt a plain/ciphered text.
         """
 
-a = RC4('abc')
+        parsed_text = ''
+        if i_format == 'hex':
+            for i in range(0, len(text), 2):
+                parsed_text += chr(int(text[i:i+2], 16))
+        elif i_format == 'ascii':
+            parsed_text = text
+        else:
+            raise Exception("Invalid input format")
+
+        converted_bytes = []
+        for char in parsed_text:
+            next_pseudo_rand = self.gen_pseudo_random()
+            converted_bytes.append(ord(char) ^ next_pseudo_rand) # apply bitwise XOR
+        
+        # Next we're concatenating while formats the bytes to hex or ASCII char.
+        if o_format == 'hex':
+            # The {:02x} is a python format especification for 2 padding hex.
+            # https://docs.python.org/3.4/library/functions.html#format
+            return ''.join(["{:02x}".format(enc) for enc in converted_bytes])
+        elif o_format == 'ascii':
+            return ''.join([chr(enc) for enc in converted_bytes])
+        else:
+            raise Exception("Invalid output format")
+    
+    def encrypt(self, text):
+        return self.apply(text, i_format='ascii', o_format='hex')
+
+    def decrypt(self, text):
+        return self.apply(text, i_format='hex', o_format='ascii')
+
+if __name__ == "__main__":
+    import sys
+    mode = sys.argv[1]
+    key = sys.argv[2]
+    text = str(sys.argv[3])
+    cipher = RC4(key)
+    if (mode == 'enc'):
+        print(cipher.encrypt(text))
+    elif (mode == 'dec'):
+        print(cipher.decrypt(text))
